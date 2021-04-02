2238307576021c31e54ba06e7e869c348faafcde
Mauricio Sugimoto <65788187+Ck320@users.noreply.github.com>
2021-04-02 16:37:25 -0300

    removendo JavaSwing

--- src/R6/Painel.java
+++ src/R6/Painel.java
@@ -5,57 +5,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Painel extends JFrame {
-
-    JFrame frame;
-    
-    JButton button;
-    JLabel oldADS,verticalFOV,aspectRatio;
-    JTextField old,vert,aspec;
-
-    public Painel() throws HeadlessException {
-
-        frame =new JFrame();
-        frame.setLayout(new GridLayout(4,2));
-        
-        oldADS = new JLabel("Old ADS");
-        verticalFOV = new JLabel("vertical FOV");
-        aspectRatio = new JLabel("aspect Ratio");
-        
-        old = new JTextField();
-        vert = new JTextField();
-        aspec = new JTextField();
-        button = new JButton("Confirmar");
-        
-        frame.add(oldADS);
-        frame.add(old);
-        
-        frame.add(verticalFOV);
-        frame.add(vert);
-        
-        frame.add(aspectRatio);
-        frame.add(aspec);
-        
-        frame.add(button);
-        
-        button.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-			double OldADS =(Double.parseDouble(old.getText()));
-			double VerticalFOV =(Double.parseDouble(vert.getText()));
-			double AspectRatio = (Double.parseDouble(aspec.getText()));
-			JOptionPane.showMessageDialog(null, new Execute().executar(VerticalFOV,AspectRatio,OldADS));
-            frame.dispose();
-			}
-		});
-        
-        frame.setVisible(true);
-        frame.setSize(300,150);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+public class Painel{
 
     public static void main(String[] args) {
-        new Painel();
+        double OldADS =50;
+        double VerticalFOV =20;
+        double AspectRatio = (4/3);
+        new Execute().executar(VerticalFOV,AspectRatio,OldADS);
     }
 }
